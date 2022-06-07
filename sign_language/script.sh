7c516181ba0aca2d61520bcb45171b23d72c6103
Karolina Kwiatkowska <kblkwiatkowska@gmail.com>
2022-06-07 13:50:17 +0200

    add binary classification dataset

--- sign_language/script.sh
+++ sign_language/script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-find 
-/Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/train/0/ 
--type f |sort -R|head -300| xargs -n 1 -I '{}' mv {} 
-/Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/test/0/
+cd /Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/
+mkdir test
+cd test/
+
+for i in {0..9}; do mkdir "$i"; done
+for i in {A..Z}; do mkdir "$i"; done
+mkdir "_"
+
+
+for i in {0..9}; do find "/Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/train/$i/" -type f |sort -R|head -300| xargs -n 1 -I '{}' mv {} "/Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/test/$i/"; done
+
+for i in {A..Z}; do find "/Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/train/$i/" -type f |sort -R|head -300| xargs -n 1 -I '{}' mv {} "/Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/test/$i/"; done
+
+find /Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/train/_/ -type f |sort -R|head -300| xargs -n 1 -I '{}' mv {} /Users/admin/KEA/4semester/ML/files/sign_language/Gesture-Image-Data/test/_/
